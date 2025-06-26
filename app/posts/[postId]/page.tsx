da04409ec26dd32cfa98a525ffa009f884c8d6f5
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-06-26 19:39:32 +0300

    Fix parameter assignment in Post component for consistency

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -2,7 +2,7 @@ import { getFetchPost } from "@/api/fetch";
 import PostCard from "@/components/PostCard";
 
 export default async function Post(props: { params: { postId: string } }) {
-  const params = await props.params;
+  const params = props.params;
   const post = await getFetchPost(params.postId);
   return (
     <div className="mt-4">
