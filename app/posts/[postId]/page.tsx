27427b9f89dc042b0e6bc394b479bdefc7043e7d
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-06-27 00:07:17 +0300

    fix problem with params (Types of property 'params' are incompatible)

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,11 +1,13 @@
 import { getFetchPost } from "@/api/fetch";
 import PostCard from "@/components/PostCard";
-interface PageProps {
-  params: { postId: string };
-}
-export default async function Post({ params }: PageProps) {
-  const awaitedParams = await params;
-  const post = await getFetchPost(awaitedParams.postId);
+
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ postId: string }>;
+}) {
+  const { postId } = await params;
+  const post = await getFetchPost(postId);
   return (
     <div className="mt-4">
       <PostCard postContent={post} />
